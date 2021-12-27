ab38f767b9d40233770995ad0be3fb3d3685ea5c
TrinadhKumarKolusu <imtrinadh@gmail.com>
2021-12-27 22:57:37 +0530

    commit-2
    add changes

---
+++ src/main/java/com/example/blog/service/CommentService.java
@@ -0,0 +1,13 @@
+package com.example.blog.service;
+
+import com.example.blog.dto.CommentDto;
+
+import java.util.List;
+
+public interface CommentService {
+    CommentDto createComment(long postId,CommentDto commentDto);
+    List<CommentDto> getCommentByPostId(long postId);
+    CommentDto getCommentById(long postId,long commentId);
+    CommentDto updateComment(long postId,long commentId,CommentDto commentRequest);
+    void deleteComment(long postId,long commentId);
+}
