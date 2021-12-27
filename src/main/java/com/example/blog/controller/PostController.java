f935dcbaabfdca3dfcc96d602a29d69f56298ada
TrinadhKumarKolusu <imtrinadh@gmail.com>
2021-12-27 18:37:04 +0530

    commit
    changes

---
+++ src/main/java/com/example/blog/controller/PostController.java
@@ -0,0 +1,50 @@
+package com.example.blog.controller;
+
+import com.example.blog.dto.PostDto;
+import com.example.blog.dto.PostResponse;
+import com.example.blog.service.PostService;
+import com.example.blog.utils.AppConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public PostResponse getAllPosts(@RequestParam(value = "pageNo",defaultValue = AppConstants.DEFAULT_PAGE_NUMBER,required = false) int pageNo,
+                                    @RequestParam(value = "pageSize",defaultValue = AppConstants.DEFAULT_PAGE_SIZE,required = false) int pageSize,
+                                    @RequestParam(value = "sortBy",defaultValue = AppConstants.DEFAULT_SORT_BY,required = false) String sortBy,
+                                    @RequestParam(value = "sortDir",defaultValue = AppConstants.DEFAULT_SORT_DIRECTION,required = false) String sortDir){
+        return postService.getAllPosts(pageNo,pageSize,sortBy,sortDir);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(value = "id") Long id){
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto,@PathVariable(value = "id") Long id){
+     PostDto postResponse=postService.updatePost(postDto,id);
+     return new ResponseEntity<>(postResponse,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(value = "id") long id){
+        postService.deletePost(id);
+        return new ResponseEntity<>("Entity deleted sucessfully",HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,96 @@
+package com.example.blog.service.impl;
+
+import com.example.blog.dto.PostDto;
+import com.example.blog.dto.PostResponse;
+import com.example.blog.exception.ResourceNotFoundException;
+import com.example.blog.model.Post;
+import com.example.blog.repository.PostRepository;
+import com.example.blog.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        Post post=mapToEntity(postDto);
+        Post newPost=postRepository.save(post);
+        PostDto postResponse=mapToDto(newPost);
+        return postResponse;
+    }
+
+    @Override
+    public PostResponse getAllPosts(int pageNo,int pageSize,String sortBy,String sortDir) {
+        Sort sort=sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+                   : Sort.by(sortBy).descending();
+        Pageable pageable= PageRequest.of(pageNo,pageSize,sort);
+        Page<Post> posts=postRepository.findAll(pageable);
+        List<Post> listOfPosts=posts.getContent();
+        List<PostDto> content= listOfPosts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+        PostResponse postResponse=new PostResponse();
+        postResponse.setContent(content);
+        postResponse.setPageNo(posts.getNumber());
+        postResponse.setPageSize(posts.getSize());
+        postResponse.setTotalElements(posts.getTotalElements());
+        postResponse.setTotalPages(posts.getTotalPages());
+        postResponse.setLast(posts.isLast());
+        return postResponse;
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+
+        Post post=postRepository.findById(id).orElseThrow(()
+                ->new ResourceNotFoundException("Post","id",id));
+        return mapToDto(post);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        Post post=postRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Post","id",id));
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        Post updatedPost=postRepository.save(post);
+        return mapToDto(updatedPost);
+    }
+
+    @Override
+    public void deletePost(long id) {
+        Post post=postRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Post","id",id));
+        postRepository.delete(post);
+    }
+
+    //convert Entity to DTO
+    private  PostDto mapToDto(Post post){
+        PostDto postDto=new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return  postDto;
+    }
+    //Convert DTO to Entity
+    private Post mapToEntity(PostDto postDto){
+        Post post=new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        return post;
+
+    }
+
+}
